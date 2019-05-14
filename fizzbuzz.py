f51138882eed442f13af470d5727ace0a55071a6
Marcin Grabowski <marcingrabowski2206@gmail.com>
2019-05-14 18:39:54 +0200

    fixed modulo bug - added not clause in both if statements.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,8 @@
 def fizzbuzz():
     for i in range(1,101):
-        if i % 3 == 0:
+        if not i % 3 == 0:
             print('fizz')
-        elif i % 5 == 0:
+        elif not i % 5 == 0:
             print('buzz')
         else:
             print(i)
